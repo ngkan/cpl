f301eea221c19d60bdd9abf23b61597d739bac09
Khanh Nguyen <ngkan146@gmail.com>
2020-12-13 10:35:39 +0100

    Update DisjointSet.h
    init more values in the constructor

--- DataStructure/DisjointSet.h
+++ DataStructure/DisjointSet.h
@@ -1,11 +1,11 @@
 /**
  * Disjoint-set data structure.
- * Status: Untested
+ * Status: Tested
  */
 
 struct DSU{
     vector <int> p, sz;
-    DSU(int n = 0): p(n), sz(n, 1)  { for(int i=0;i<n;i++) p[i] = i;}
+    DSU(int n = 0): p(n+1), sz(n+1, 1)  { for(int i=0;i<=n;i++) p[i] = i;}
     int find(int x)     { return x == p[x] ? x : p[x] = find(p[x]); }
     int get_size(int x) { return sz[find(x)]; }
     void merge(int x,int y){   
